ea1aa46cbc61fb8a73c65b86e7ca2d70241fd278
Farid Koushaneh <faridko@gmail.com>
2022-10-07 12:23:31 -0700

    Update Chat.c
    changed main and IP function

--- Chat.c
+++ Chat.c
@@ -7,45 +7,64 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define SERVER_ADDR "172.217.160.99"
-#define SERVER_PORT 80
-char myIP[16];
-unsigned int myPort;
-struct sockaddr_in server_addr, my_addr;
-int sockfd;
+
+
+
 
 void IP()
 {
 
+  char myIP[16];
+  struct sockaddr_in server_addr, my_addr;
+  int sockIP;
+ #define SERVER_ADDR "172.217.160.99"
+ #define SERVER_PORT 80
+
+// Clean buffers:
+  bzero(&server_addr, sizeof(server_addr));
+
+//Create socket
+  sockIP = socket(AF_INET, SOCK_STREAM, 0);
+
+
+  if (sockIP < 0) {
+    perror("Can't open stream socket.");
+    exit(-1);
+    return;
+  }
+
+
+  // Set server_addr
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+  server_addr.sin_port = htons(SERVER_PORT);
+
+  // Send connection request to server:
+  if (connect(sockIP, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+    perror("Connect server error");
+    close(sockIP);
+    exit(-1);
+    return;
+  }
+
   // Get my ip address
+  // Clean buffers:
   bzero(&my_addr, sizeof(my_addr));
   socklen_t len = sizeof(my_addr);
-  getsockname(sockfd, (struct sockaddr *) &my_addr, &len);
+
+  //retrieve the current address of the sockIP socket, store this address in the sockaddr_in structure (my_addr)
+  getsockname(sockIP, (struct sockaddr *) &my_addr, &len);
+
+  //converts a numeric network address in my_addr.sin_addr into a text string and store in myIP
   inet_ntop(AF_INET, &my_addr.sin_addr, myIP, sizeof(myIP));
-  myPort = ntohs(my_addr.sin_port);
+
 
   printf("The ip address is %s\n", myIP);
 
-
-
-}
-
-
-
-void Port() {
-
-  // Get my port
-  bzero(&my_addr, sizeof(my_addr));
-  socklen_t len = sizeof(my_addr);
-  getsockname(sockfd, (struct sockaddr *) &my_addr, &len);
-  //inet_ntop(AF_INET, &my_addr.sin_addr, myIP, sizeof(myIP));
-  myPort = ntohs(my_addr.sin_port);
-
-
-  printf("The program runs on port number %u\n", myPort);
-
+  close(sockIP);
 
 }
+
 
 
 void Help()
@@ -57,57 +76,67 @@ void Help()
 }
 
 
-int main() {
+int main(int argc, char* argv[]) {
 
-
-  // Connect to server
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("Can't open stream socket.");
-    exit(-1);
+  if (argc < 2) {
+    fprintf(stderr, "ERROR, no port provided\n");
+    exit(1);
   }
 
-  // Set server_addr
-  bzero(&server_addr, sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
-  server_addr.sin_port = htons(SERVER_PORT);
+  int port;
+  port = atoi(argv[1]);
+
+  char cmd[1024];
+  char *arg;
+  char array[3][32]; // to store arguments from command line 
+
+  while (fgets(cmd, 1024, stdin)) {
 
 
-  if (connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-    perror("Connect server error");
-    close(sockfd);
-    exit(-1);
-  }
+    cmd[strlen(cmd) - 1] = '\0';
+    arg = strtok(cmd, " ");
+    int i = 0;
 
-  char choice[12];
+    while (arg != NULL) {
 
+      strcpy(array[i], arg);
+      i += 1;
+      arg = strtok(NULL, " ");
 
-  while (strcmp(choice, "exit") != 0) {
-    scanf("%s", choice);
-    if (strcmp(choice, "help") == 0) {
+    }
+    if (i>3){
+      fprintf(stderr, "ERROR, Number of arguments should be 3\n");
+      continue;
+  
+    }
+
+    if (!(strcmp(array[0], "help"))) {
       Help();
     }
-    else if (strcmp(choice, "myip") == 0) {
+    else if (!(strcmp(array[0], "myip"))) {
       IP();
     }
-    else if (strcmp(choice, "myport") == 0) {
-      Port();
+    else if (!(strcmp(array[0], "myport"))) {
+      printf("The program runs on port number %u\n", port);
     }
-    else if (strcmp(choice, "connect") == 0) {
+    else if (!(strcmp(array[0], "connect"))) {
       //connect function
+      //ip address stored in array[1]
+      //port number stored in array[2]
     }
-    else if (strcmp(choice, "send") == 0) {
+    else if (!(strcmp(array[0], "send"))) {
       //send function
     }
-    else if (strcmp(choice, "list") == 0) {
+    else if (!(strcmp(array[0], "list"))) {
       //list function
     }
-    else if (strcmp(choice, "terminate") == 0) {
+    else if (!(strcmp(array[0], "terminate"))) {
       //terminate function
     }
+
   }
 
-  close(sockfd);
   exit(-1);
   return 0;
+
 }
